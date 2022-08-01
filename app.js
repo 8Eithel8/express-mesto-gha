1d36bc8e1eff98122324bb2f94d9b0818a02748c
8Eithel8 <eithel@list.ru>
2022-08-01 13:20:13 +0300

    feat: create auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { NOT_FOUND } = require('./controllers/errors');
 const { login, createUser } = require('./controllers/user');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,11 +17,13 @@ mongoose.connect(
   { useNewUrlParser: true },
 );
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
 app.post('/signin', login);
 app.post('/signup', createUser);
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: 'Путь не найден' });
